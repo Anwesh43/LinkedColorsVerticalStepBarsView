63f4b2a4e72e289ec60971b94a0f8d0afe07ad63
Anwesh43 <anweshthecool0@gmail.com>
2020-02-01 11:54:56 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcolorverticalstepbarsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcolorverticalstepbarsview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcolorverticalstepbarsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.colorverticalstepbarsview.ColorVerticalStepBarsView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ColorVerticalStepBarsView.create(this)
     }
 }
