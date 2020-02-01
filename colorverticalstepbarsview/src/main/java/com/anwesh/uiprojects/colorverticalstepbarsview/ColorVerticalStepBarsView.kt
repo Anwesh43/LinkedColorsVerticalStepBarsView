b2d4521ded18f3f5145026ab3f00d0abe04b20fc
Anwesh43 <anweshthecool0@gmail.com>
2020-02-01 11:43:08 +0530

    adding CVSBNode class

--- colorverticalstepbarsview/src/main/java/com/anwesh/uiprojects/colorverticalstepbarsview/ColorVerticalStepBarsView.kt
+++ colorverticalstepbarsview/src/main/java/com/anwesh/uiprojects/colorverticalstepbarsview/ColorVerticalStepBarsView.kt
@@ -114,4 +114,46 @@ class ColorVerticalStepBarsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CVSBNode(var i : Int, val state : State = State()) {
+
+        private var next : CVSBNode? = null
+        private var prev : CVSBNode? = null
+
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CVSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCVSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CVSBNode {
+            var curr : CVSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
