4e14af963543fd64dcbcc678245b63d13963beac
Anwesh43 <anweshthecool0@gmail.com>
2020-02-01 11:03:29 +0530

    adding constants and libraries

--- colorverticalstepbarsview/src/main/java/com/anwesh/uiprojects/colorverticalstepbarsview/ColorVerticalStepBarsView.kt
+++ colorverticalstepbarsview/src/main/java/com/anwesh/uiprojects/colorverticalstepbarsview/ColorVerticalStepBarsView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.colorverticalstepbarsview
  * Created by anweshmishra on 01/02/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#3F51B5", "#880E4F", "#009688", "#f44336", "#4CAF50")
+val bars : Int = 5
+val scGap : Float = 0.02f
+val sizeFactor : Float = 5.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
